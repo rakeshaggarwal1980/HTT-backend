d6daa87435592b6749745d807ade10d8cf03b0f4
Shikha <shika.basra@evry.com>
2020-08-25 19:49:25 +0530

    Changes in Request Model ComeToOfficeRequest.cs and create request method

---
+++ Migrations/20200825132033_rmv-empCode-add-comments.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HTTAPI.Migrations
+{
+    public partial class rmvempCodeaddcomments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeCode",
+                table: "ComeToOfficeRequest");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HRComments",
+                table: "ComeToOfficeRequest",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HRComments",
+                table: "ComeToOfficeRequest");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeCode",
+                table: "ComeToOfficeRequest",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
