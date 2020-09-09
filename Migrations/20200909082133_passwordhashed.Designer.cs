16182c40c9fe32acdccf44bd960c9fa1bfd9a50c
Shikha <shika.basra@evry.com>
2020-09-09 14:16:45 +0530

    Store and read hash password

--- Migrations/20200909082133_passwordhashed.Designer.cs
+++ Migrations/20200909082133_passwordhashed.Designer.cs
@@ -4,14 +4,16 @@ using HTTAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HTTAPI.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20200909082133_passwordhashed")]
+    partial class passwordhashed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
